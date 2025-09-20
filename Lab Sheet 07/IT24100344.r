3fbfa13ea48a5f3dc5e107b6daa5b57597b05bf7
IT24100344 <IT24100344@my.sliit.lk>
2025-09-20 22:55:11 +0530

    Lab_07

---
+++ Lab Sheet 07/IT24100344.r
@@ -0,0 +1,11 @@
+setwd("D:\\SLIIT\\Year2_Sem1\\IT2120 Probability and Statistics\\IT24100344")
+#Q1
+punif(15,min = 0, max = 40, lower.tail = TRUE)
+#Q2
+pexp(2,rate = 0.333, lower.tail = TRUE)
+#Q3)i
+1-pnorm(130,mean = 100, sd=15, lower.tail = TRUE)
+#Q3)ii
+qnorm(0.95, mean = 100, sd = 15)
+
+
